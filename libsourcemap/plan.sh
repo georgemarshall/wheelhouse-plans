f336255d774e86e4e946a7defb7b8a55d438c102
George Marshall <george@georgemarshall.name>
2016-12-13 11:31:47 -0800

    Changed libsourcemap to use tar.gz as its preferred format

--- libsourcemap/plan.sh
+++ libsourcemap/plan.sh
@@ -6,8 +6,8 @@ pkg_dirname=${python_name}-${pkg_version}
 #pkg_source=https://pypi.org/packages/source/l/libsourcemap/${pkg_dirname}.zip
 #pkg_shasum=63627489b9d0bc2111751b2bdccff0bc91d029eefe58c3a2842c364fc4530808
 # FIXME: Workaround for https://github.com/getsentry/libsourcemap/issues/5
-pkg_source=https://github.com/getsentry/libsourcemap/archive/${pkg_version}.zip
-pkg_shasum=85c4b058f1965dc362c9fbd17a623de903f768b7b62227e74ecd40e42fdce0fa
+pkg_source=https://github.com/getsentry/libsourcemap/archive/${pkg_version}.tar.gz
+pkg_shasum=2b8e0a17b1af0d13496ca42d8fed61cdf11911c339f22b2f0288d4d7f4d0cf46
 pkg_maintainer="George Marshall <george@georgemarshall.name>"
 pkg_description="The Web framework for perfectionists with deadlines."
 pkg_upstream_url=https://python-pillow.org/
